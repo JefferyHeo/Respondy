c588cdeb7dca5cc049c96fd3d81a13a9914ae24a
Ahn-Jaehun <jeahun2625@gmail.com>
2026-05-11 15:35:58 +0900

    Register AI coaching API routes

--- config/urls.py
+++ config/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("conversations.urls")),
+    path("api/coaching/", include("ai_coaching.urls")),
 ]
 
 if settings.DEBUG:
